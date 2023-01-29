a6949d0091dee63cdb9d9b2e9a05a39b1e09cafc
albertoBarl <alberto.barletta00@gmail.com>
2023-01-29 18:20:44 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+<body>
+    <h1>Esegui il Login</h1>
+    <form action="./accesso.php" method="POST">
+        <input type="text" name="username" placeholder="Inserisci l'username">
+        <input type="password" name="password" placeholder="Inserisci la password">
+        <button type="submit">Esegui accesso</button>
+    </form>
+</body>
+</html>
