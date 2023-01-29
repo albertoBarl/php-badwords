53856f9a71425b21ffd79c34f7a4363b7bedffbf
albertoBarl <alberto.barletta00@gmail.com>
2023-01-29 18:48:23 +0100

    fixed document title

--- accesso.php
+++ accesso.php
@@ -12,7 +12,7 @@ $censoredpassword=str_replace($password, "*****...", $password);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Accesso effettuato</title>
 </head>
 <body>
     <h1>Benvenuto "<?php echo $username?>"</h1>
